7a31915d8c7ee01cd0721f6b72acf10cf161c54c
Swastik <swastikbasu626@gmail.com>
2026-06-05 11:01:41 +0530

    For Day 1 initialised my project structure along with the required files. The codes for the following days are to be made accordingly

---
+++ src/main/java/com/signpdf/SignpdfApplication.java
@@ -0,0 +1,13 @@
+package com.signpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignpdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignpdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signpdf/SignpdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignpdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
