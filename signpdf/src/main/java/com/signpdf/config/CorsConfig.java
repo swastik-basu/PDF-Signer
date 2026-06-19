3c7fec57a39ab81498133127b889cd8f6daaebf3
Swastik <swastikbasu626@gmail.com>
2026-06-19 20:25:53 +0530

    Fix CORS

--- signpdf/src/main/java/com/signpdf/config/CorsConfig.java
+++ signpdf/src/main/java/com/signpdf/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.setAllowedOrigins(List.of("http://localhost:5173" , "https://sign-buddy-neon.vercel.app/login"));
+		configuration.setAllowedOrigins(List.of("http://localhost:5173" , "https://sign-buddy-neon.vercel.app"));
 
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
